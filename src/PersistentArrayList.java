39f0138b11662ed30df717ca96d6baf203d9984f
17solan_d <17solan_d@bexleygs.co.uk>
2023-01-19 11:41:21 +0000

    V8, persistentarraylist class framework is present

---
+++ src/PersistentArrayList.java
@@ -0,0 +1,30 @@
+public class PersistentArrayList {
+
+    public static void New(){
+
+    }
+
+    public static int get(int i){
+        return 0;
+    }
+
+    public static void add(String element){
+
+    }
+
+    public static void clear(){
+
+    }
+
+    public static int size(){
+        return 0;
+    }
+
+    public static boolean contains(String element){
+        return true;
+    }
+
+    public static int indexOf(String element){
+        return 0;
+    }
+}
